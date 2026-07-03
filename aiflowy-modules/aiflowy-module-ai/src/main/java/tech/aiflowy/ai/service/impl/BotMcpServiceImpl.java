a6e68d0243438e30f4583f46d0808e854ee24173
Berlin <liangbl@dist.com.an>
2026-07-03 16:22:08 +0800

    fix:Batch saving of botMcp

--- aiflowy-modules/aiflowy-module-ai/src/main/java/tech/aiflowy/ai/service/impl/BotMcpServiceImpl.java
+++ aiflowy-modules/aiflowy-module-ai/src/main/java/tech/aiflowy/ai/service/impl/BotMcpServiceImpl.java
@@ -1,14 +1,20 @@
 package tech.aiflowy.ai.service.impl;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
 import tech.aiflowy.ai.entity.BotMcp;
 import tech.aiflowy.ai.mapper.BotMcpMapper;
 import tech.aiflowy.ai.service.BotMcpService;
 import org.springframework.stereotype.Service;
+import tech.aiflowy.common.audio.socket.AudioSocketHandler;
+import tech.aiflowy.log.annotation.LogRecord;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -22,29 +28,44 @@ import java.util.Map;
 @Service
 public class BotMcpServiceImpl extends ServiceImpl<BotMcpMapper, BotMcp>  implements BotMcpService{
 
+    private final static Logger log = LoggerFactory.getLogger(BotMcpServiceImpl.class);
+
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public void updateBotMcpToolIds(BigInteger botId, List<Map<String, List<List<String>>>> mcpSelectedData) {
         // 删除原来绑定的mcp
         this.remove(QueryWrapper.create().eq(BotMcp::getBotId, botId));
+        if (mcpSelectedData == null || mcpSelectedData.isEmpty()) {
+            return;
+        }
+        List<BotMcp> list = new ArrayList<>();
         for (Map<String, List<List<String>>> mcpItem : mcpSelectedData) {
+            if (mcpItem == null) {
+                continue;
+            }
             for (Map.Entry<String, List<List<String>>> entry : mcpItem.entrySet()) {
-                String mcpId = entry.getKey(); // 上一级id
-                List<List<String>> toolList = entry.getValue(); // 包含name和description的二维数组
-
-                // 遍历每个工具的[name, description]
+                String mcpId = entry.getKey();
+                List<List<String>> toolList = entry.getValue();
+                if (toolList == null || toolList.isEmpty()) {
+                    continue;
+                }
+                //复用mcpId
+                BigInteger mcpIdValue = new BigInteger(mcpId);
                 for (List<String> toolInfo : toolList) {
-                    String toolName = toolInfo.get(0); // 工具名称
-                    String toolDesc = toolInfo.get(1); // 工具描述
-                    System.out.println("工具名称：" + toolName + "，描述：" + toolDesc);
-                    BotMcp botMcp = new BotMcp();
-                    botMcp.setBotId(botId);
-                    botMcp.setMcpId(new BigInteger(mcpId));
-                    botMcp.setMcpToolName(toolName);
-                    botMcp.setMcpToolDescription(toolDesc);
-                    this.save(botMcp);
+                    if(CollectionUtil.isNotEmpty(toolInfo)){
+                        BotMcp botMcp = new BotMcp();
+                        botMcp.setBotId(botId);
+                        botMcp.setMcpId(mcpIdValue);
+                        botMcp.setMcpToolName(toolInfo.get(0));
+                        botMcp.setMcpToolDescription(toolInfo.get(1));
+                        list.add(botMcp);
+                        log.info("工具名称：" + toolInfo.get(0) + "，描述：" + toolInfo.get(1));
+                    }
                 }
             }
+        }
+        if (!list.isEmpty()) {
+            this.saveBatch(list);
         }
     }
 }
